2562a204aa343d0db4064c613239e27b3d58e4c1
Edward M Banner <bannerem195@potsdam.edu>
2012-09-23 22:12:09 -0400

    software engineering story script

---
+++ story.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# allows the user to easily create a story
+
+echo -n "Story Name: "
+read
+story_name=$REPLY
+
+echo -n "As a: "
+read
+as_a=$REPLY
+
+echo -n "I want: "
+read
+i_want=$REPLY
+
+echo -n "So that: "
+read
+so_that=$REPLY
+
+echo
+echo -n "Acceptance Criteria: "
+read
+acceptance_criteria=$REPLY
+
+echo -n "Given: "
+read
+given=$REPLY
+
+echo -n "When: "
+read
+when=$REPLY
+
+echo -n "Then: "
+read
+Then=$REPLY
+
+echo
+echo "Story Name: ${story_name}"
+echo "As a ${as_a},"
+echo "I want $i_want"
+echo "So that ${so_that}."
+echo
+echo "Acceptance Criteria:"
+echo "Given ${given},"
+echo "When ${when}"
+echo "Then ${Then}."
