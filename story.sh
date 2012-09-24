168bb2c265fed12257551d8721ac826ee79087b0
Edward M Banner <bannerem195@potsdam.edu>
2012-09-24 13:19:23 -0400

    updated story.sh to generate HTML code

--- story.sh
+++ story.sh
@@ -2,9 +2,20 @@
 
 # allows the user to easily create a story
 
+TD='<td>'
+ETD='</td>'
+
 echo -n "Story Name: "
 read
 story_name=$REPLY
+
+echo -n "Size: "
+read
+size=$REPLY
+
+echo -n "Priority: "
+read
+priority=$REPLY
 
 echo -n "As a: "
 read
@@ -35,15 +46,17 @@ echo -n "Then: "
 read
 Then=$REPLY
 
-echo
-echo "Story Name: ${story_name}"
-echo "As a ${as_a},"
-echo "I want $i_want"
-echo "So that ${so_that}."
-echo
-echo "Acceptance Criteria:"
-echo "Given ${given},"
-echo "When ${when}"
-echo "Then ${Then}."
+echo '<tr>'
+echo "$TD${story_name}$ETD"
+echo "$TD${size}$ETD"
+echo "$TD${priority}$ETD"
+echo "$TD${as_a}$ETD"
+echo "$TD${i_want}$ETD"
+echo "$TD${so_that}$ETD"
+echo "$TD${acceptance_criteria}$ETD"
+echo "$TD${given}$ETD"
+echo "$TD${when}$ETD"
+echo "$TD${Then}$ETD"
+echo '</tr>'
 
 exit 0
