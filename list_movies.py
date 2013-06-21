58f95efabb72dc862cc3923b4f871e8937478902
Edward Banner <edward.banner@gmail.com>
2013-06-21 17:10:19 -0400

    Added verbose option to the movie locator.

--- list_movies.py
+++ list_movies.py
@@ -16,21 +16,23 @@ extensions = (
     'rm',
     'rmvb',
 )
-movies = []
-output_file = 'movies.list'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a list of movies')
-    parser.add_argument('movie_dir', help='The parent directory of your movies')
-    parser.add_argument(
-        '-o', '--output-file', 
-        help='The name of a file that will contain the movies',
-        default='movies.list'
+    parser.add_argument('movie_dir', help='the parent directory of your movies')
+    parser.add_argument('-v', '--verbose',
+            help='increase verbosity',
+            action='store_true'
     )
     parser.add_argument(
-        '-e', '--extension-file', 
-        help='A file containing the extensions the user would like to have '
-        'included in his or her movie search'
+            '-o', '--output-file', 
+            help='the name of a file that will contain the movies',
+            default='movies.list'
+    )
+    parser.add_argument(
+            '-e', '--extension-file', 
+            help='a file containing the extensions the user would like to have '
+            'included in his or her movie search'
     )
     args = parser.parse_args()
 
@@ -44,13 +46,14 @@ if __name__ == '__main__':
 
     # Open the file for writing
     try:
-        f = open(args.output_file, 'w')
+        with open(args.output_file, 'w') as f:
+            # Write all movies that end with allowed extensions
+            for root, dirs, files in os.walk(args.movie_dir):
+                for file in files:
+                    if file.split('.')[-1].strip().lower() in extensions:
+                        f.write(file + "\n")
+                        if args.verbose:
+                            print(file)
     except IOError:
         print('Could not open {}. Exiting.'.format(args.output_file))
         sys.exit()
-
-    # Write all movies that end with allowed extensions
-    for root, dirs, files in os.walk(args.movie_dir):
-        for file in files:
-            if file.split('.')[-1].strip().lower() in extensions:
-              f.write(file + "\n")
